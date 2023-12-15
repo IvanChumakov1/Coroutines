54773e8c7710295bc3db240a5519e117705dc917
IvanChumakov1 <ivanchumakov228@gmail.com>
2023-12-16 03:36:31 +0700

    Add coroutine

--- app/src/main/java/com/hfad/coroutines/MainActivity.kt
+++ app/src/main/java/com/hfad/coroutines/MainActivity.kt
@@ -2,10 +2,50 @@ package com.hfad.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import com.hfad.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    suspend fun performTask(tasknumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5_000)
+            return@async "Finished Coroutine ${tasknumber}"
+        }
+    private  lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
+    fun launchCoroutines(view: View) {
+        (1..count).forEach {
+            binding.statusText.text = "Started Coroutine ${it}"
+            coroutineScope.launch(Dispatchers.Main) {
+                binding.statusText.text = performTask(it).await()
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                count = progress
+                binding.countText.text = "${count} coroutines"
+            }
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+            }
+            override fun onStopTrackingTouch(seek: SeekBar) {
+
+            }
+        })
     }
+
 }
